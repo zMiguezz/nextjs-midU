a6157cc50ad3381153cbedbe5ca1d4a27ae0cfb7
zMiguezz <hector_miguel_moreno_gonzalez@outlook.com>
2023-12-25 22:01:36 -0600

    skeleton component and fetch consts

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,9 +1,11 @@
-import { fetchRevenue } from "../lib/data"
+import { Suspense } from "react"
+import { fetchLatestInvoices, fetchRevenue } from "../lib/data"
+import LatestInvoices from "../ui/dashboard/latest-invoices"
 import RevenueChart from "../ui/dashboard/revenue-chart"
 import { lusitana } from "../ui/fonts"
 
     export default async function DashboardPage() {
-    const revenue = await fetchRevenue()
+    const latestInvoices = await fetchLatestInvoices()
     return (
         <main>
         <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
@@ -20,8 +22,10 @@ import { lusitana } from "../ui/fonts"
           /> */}
         </div>
         <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-          <RevenueChart revenue={revenue}  />
-          {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
+          <Suspense fallback={<div>Cargando...</div>}>
+            <RevenueChart revenue={revenue}  />
+          </Suspense>
+          <LatestInvoices latestInvoices={latestInvoices} />
         </div>
       </main>
     )
